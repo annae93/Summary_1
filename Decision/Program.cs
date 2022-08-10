fd9fff626a6d92d5aacea523de3637d899b73c37
Anna <anna.a.enina@yandex.ru>
2022-08-10 14:10:54 +0300

    Add decision

---
+++ Decision/Program.cs
@@ -0,0 +1,35 @@
+﻿string[] GetShorterStrings(string[] inputStrings)
+{
+    int inputSize = inputStrings.Length;
+    
+    int[] indexes = new int[inputSize];
+    
+    int outputSize = 0;
+
+    for (int i = 0; i < inputSize; i++)
+    {
+        if (inputStrings[i].Length <= 3)
+        {
+            indexes[outputSize] = i;
+            outputSize++;
+        }
+    }
+
+    string[] result = new string[outputSize];
+
+    for (int i = 0; i < outputSize; i++)
+    {
+        result[i] = inputStrings[indexes[i]];
+    }
+
+    return result;
+}
+
+string[] strings = {"hello", "world", "-2", ":-)"};
+
+string[] results = GetShorterStrings(strings);
+
+for (int i = 0; i < results.Length; i++)
+{
+    Console.WriteLine(results[i]);
+}
